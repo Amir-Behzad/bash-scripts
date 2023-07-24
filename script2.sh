8cea5462663d9ad8bf255c470ce891bc5c36b62f
Amir-Behzad <a.behzad.ebr@gmail.com>
2023-07-24 09:09:35 -0400

    adding to array and indexing it

--- script2.sh
+++ script2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 myArr=(one two three four five)
-echo "odd numbers: ${myArr[0]}, ${myArr[2]}, ${myArr[4]}"
+myArr+=(six seven)
+echo "odd numbers: ${myArr[0]}, ${myArr[2]}, ${myArr[4]}, ${myArr[-1]}"
 exit 0
