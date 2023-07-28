1eca524b7d93f9a151b720a8b4507006051833ed
Amir-Behzad <a.behzad.ebr@gmail.com>
2023-07-28 22:05:53 +0330

    explore echo command

--- script6.sh
+++ script6.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
-echo 
+# echo 
 
 # The variable is known to be "naked", when it is being assignd.
 # In that case it would be lacking  a "$" in front.
 
 # Assignment:
-a=23
-echo "The value of \"a\" is $a"
+# a=23
+# echo "The value of \"a\" is $a"
 
-echo
+# echo
 
 # Assignment using 'let' syntax:
-let a=23+2
-echo "The value of \"a\" is incremented by two: $a"
+# let a=23+2
+# echo "The value of \"a\" is incremented by two: $a"
 
-echo
+# echo
 
 
 # In a 'for' loop (really, a type of dicguised assignment):
 # the -n flag in echo command results in not adding a newline at the end.
 
-echo -n "Values of \"a\" in the loop are: "
-for a in 1 3 5 7 9 11
-do
-    echo -n "$a "
-done 
+# echo -n "Values of \"a\" in the loop are: "
+# for a in 1 3 5 7 9 11
+# do
+#     echo -n "$a "
+# done 
 
-echo
-echo
+# echo
+# echo
 
 # In a 'read' statement (also a type of assignment):
-echo -n "Enter \"name\": "
-read name
-echo -e "Hello $name."
+# echo -n "Enter \"name\": "
+# read name
+# echo -e "Hello $name."
+
+# Using command substitutin in variable assignment.
+
+a=`echo Hello!`
+echo $a
+
+a=`ls -lAh`
+# Unquoted variables lack the white spaces.
+echo $a
+echo 
+# Quoted variable preserves whitespace.
+echo "$a"
+
+exit 0
