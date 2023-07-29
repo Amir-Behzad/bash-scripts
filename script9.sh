fd917dc63cd7826b3dfeaf97c7c942ea679fbfd6
Amir-Behzad <a.behzad.ebr@gmail.com>
2023-07-29 11:21:53 +0330

    run again

--- script9.sh
+++ script9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-myfile=`echo "Third attempt" >> ./sandbox/text-$(date +%y-%m-%d).txt`
+myfile=`echo "Third attempt" >> ./sandbox/text2-$(date +%y-%m-%d).txt`
 touch ./sandbox/myfile
 
 # ** the ">" sign replaces the content with the value ** 
